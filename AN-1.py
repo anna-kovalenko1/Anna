7645411847d5d30c3a42ccabbd57849b8ca588d2
anna-qa <anna.kovalenko1994@gmail.com>
2020-02-29 21:00:24 +0200

    Change1

--- AN-1.py
+++ AN-1.py
@@ -1,11 +1,10 @@
 print "Hello,Serhii"
-print "Hello everybody"
-print "For exit write e"
 while 1:
     print "Write two numbers"
     a = input()
     b = input()
     s = a + b
     print "Sum", s
+    print "For exit write e"
 if a==e:
     sys.exit(0)
